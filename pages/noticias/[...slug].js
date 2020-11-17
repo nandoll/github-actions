87761cc7cd49af34c97d09648f06e2712e77fef6
Fernando Antezana <fernando.antezana@hundred.com.pe>
2020-11-17 08:48:23 -0500

    Cambio en la logica de getStaticPaths

--- pages/noticias/[...slug].js
+++ pages/noticias/[...slug].js
@@ -6,14 +6,15 @@ import parse from 'html-react-parser'
 
 
 function Noticia ({ post }) {
-  const router = useRouter(); 
-  const { _id, slug } = router.query
-  const handleResponse = ( info ) => {
-    const {items} = info
-    const { [0]:detalle } = items
-    const { ["post-body"]:contenidoDetalle  } = detalle
-    return contenidoDetalle
+  const router = useRouter();
+
+  if(router.isFallback){
+    return <div>Loading...</div>
   }
+
+  
+  
+
   return (
     <MainLayout>
       <section className="text-gray-700 body-font">
@@ -23,48 +24,50 @@ function Noticia ({ post }) {
               Lateral
             </div>
             <div className="lg:w-3/4 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0 -mt-4" style={{border: "1px solid green"}}>
-              {
-                parse(handleResponse(post))
-              }
+              {post.items[0].name}
             </div>
           </div>          
         </div>
       </section>
       
-     {slug}
+     
     </MainLayout>
   )
 }
-
-
-export async function getStaticPaths() {  
-  const res = await fetch(`https://api.webflow.com/collections/5fa2c45087b41f0f9b713464/items?api_version=1.0.0&access_token=ed2770ed568f942e403fab9300fa760b97eadc3ea3bb5901e025deb8cd4cb3ee`)
-  const posts = await res.json()
-  const { items:noticias } = posts
-
-  // Get the paths we want to pre-render based on posts
-  const paths = noticias.map((post) => ({
-    params: { slug: [post.slug], id: post._id },         
-  }))
-
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
-  return { paths, fallback: true }
-  
-  
-}
-
 // This also gets called at build time
 export async function getStaticProps({ params }) {
   // params contains the post `id`.  
   const [,id] = params.slug  
+  
   
   // If the route is like /posts/1, then params.id is 1
   const res = await fetch(`https://api.webflow.com/collections/5fa2c45087b41f0f9b713464/items/${id}?api_version=1.0.0&access_token=ed2770ed568f942e403fab9300fa760b97eadc3ea3bb5901e025deb8cd4cb3ee`)
   const post = await res.json()
 
   // Pass post data to the page via props
-  return { props: { post } }
+  
+  return { props: {post}  }
+}
+
+export async function getStaticPaths() {  
+  const res = await fetch(`https://api.webflow.com/collections/5fa2c45087b41f0f9b713464/items?api_version=1.0.0&access_token=ed2770ed568f942e403fab9300fa760b97eadc3ea3bb5901e025deb8cd4cb3ee`)
+  const posts = await res.json()
+  const { items:noticias } = posts
+
+  // // Get the paths we want to pre-render based on posts
+  const paths = noticias.map((post) => ({
+    params: { 
+      slug: [post.slug, post._id ]
+    },         
+  }))
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  
+  return { 
+    fallback: true ,
+    paths,
+  }    
 }
 
 
