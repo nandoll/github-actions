a4dcac7dd9c206b58259dbc4cd6aaa4d5cfcb8b2
Fernando Antezana <fernando.antezana@hundred.com.pe>
2020-11-18 16:50:09 -0500

    fix url

--- components/ui/card/PostCard.js
+++ components/ui/card/PostCard.js
@@ -14,12 +14,11 @@ export const PostCard = ( { post, cats} ) => {
     const { [0]:categoria } = resultado
     const { name } = categoria    
     return name
-  }
-  
+  }  
   return (
     <div className="p-4 md:w-1/3">
       <div className="h-full shadow-2xl border-gray-200 rounded-card overflow-hidden">
-        <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={image.url} alt="blog"/>
+        <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={image?.url} alt="blog"/>
         <div className="p-6">
           <h2 className="uppercase tracking-widest text-xs title-font font-medium text-gray-500 mb-1">{ formatDistanceFromNow( new Date(creacion), {locale:es})}</h2>
           <Link href={`/noticias/${encodeURIComponent(slug)}/${encodeURIComponent(_id)}`} as={`/noticias/${encodeURIComponent(slug)}/${encodeURIComponent(_id)}`} >              
