55de131411d58c3b8373df7c1458c3d8d75bea9f
Fernando Antezana <fernando.antezana@hundred.com.pe>
2020-11-18 21:05:19 -0500

    Agregando secciones y enlaces paginas faltantes

--- components/ui/Navbar/MainNavbar.js
+++ components/ui/Navbar/MainNavbar.js
@@ -4,9 +4,14 @@ import Link from 'next/link'
 export const MainNavbar = () => {
     return (
       <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link className="mr-5 hover:text-gray-900" href="/"> Home  </Link>
-            {/* <Link className="mr-5 hover:text-gray-900" href="/nuestra-gente">Nuestra gente</Link>             */}
-            <Link className="mr-5 hover:text-gray-900" href="/noticias">Noticias</Link>                        
+            <Link className="mr-5 hover:text-gray-900" href="/"> Portada  </Link>
+            <Link className="mr-5 hover:text-gray-900" href="/proposito"> Nuestro propósito  </Link>
+            <Link className="mr-5 hover:text-gray-900" href="/nuestra-gente">Nuestra gente</Link>            
+            <Link className="mr-5 hover:text-gray-900" href="/innovacion"> Innovación de impacto </Link>
+            <Link className="mr-5 hover:text-gray-900" href="/aprendizaje"> Aprendizaje continuo  </Link>
+            <Link className="mr-5 hover:text-gray-900" href="/noticias"> En los medios</Link>                        
+            <Link className="mr-5 hover:text-gray-900" href="/"> Información corporativa </Link>
+            <Link className="mr-5 hover:text-gray-900" href="/"> Enterate.SomosIntercorp.net</Link>
         </nav>
     )
 }
