1c6a9d7618ebe832b51f89f47e9b043cb844229d
Fernando Antezana <fantezana@outlook.com>
2020-11-27 08:42:33 -0500

    Errro build verbose

--- pages/noticias/[...slug].js
+++ pages/noticias/[...slug].js
@@ -9,7 +9,7 @@ import {es} from 'date-fns/locale'
 import parse from 'html-react-parser'
 
 
-function Noticia ({ post }) {
+function Noticia ({ items }) {
   const router = useRouter();
   //ojo: slug[0:categoria - 1:slug - 2:id]
   // const { [0]:categoria } = router.query
@@ -20,7 +20,7 @@ function Noticia ({ post }) {
     return <div>Loading...</div>
   }
 
-  const { name, ["created-on"]:creacion, ["post-body"]:cuerpo, ['main-image']:ruta, categoria } = post.items[0]
+  const { name, ["created-on"]:creacion, ["post-body"]:cuerpo, ['main-image']:ruta, categoria } = items
   
   
 
@@ -59,13 +59,12 @@ function Noticia ({ post }) {
 }
 // This also gets called at build time
 
-export async function getStaticProps({ params, locale, locales }) {
+export async function getStaticProps({ params}) {
 
   const [,id] = params?.slug;
   const res = await fetch(`https://api.webflow.com/collections/5fa2c45087b41f0f9b713464/items/${id}?api_version=1.0.0&access_token=ed2770ed568f942e403fab9300fa760b97eadc3ea3bb5901e025deb8cd4cb3ee`);
   const post = await res.json();
-  const [items] = post?.items
-  
+  const [items] = post?.items   
   return { props: {items}  }
 }
 
