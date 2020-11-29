71b612b40f55fb1a36a6f6850f3c399a72a773c0
Fernando Antezana <fantezana@outlook.com>
2020-11-29 12:14:40 -0500

    Fix bugs in [...slug]

--- pages/noticias.js
+++ pages/noticias.js
@@ -4,22 +4,19 @@ import { MainLayout } from '../components/ui/layout/MainLayout'
 import { CategoriesLeftSidebar } from '../components/ui/sidebar/CategoriesLeftSidebar'
 
 
-function Noticias({ posts, cats }){
-  
-  const { items:noticias, count, total, limit } = posts
-  const { items:categorias } = cats
-  
-  
-  
+function Noticias({ posts, cats }) {
+
+  const { items: noticias, count, total, limit } = posts
+  const { items: categorias } = cats
 
   const handleChangeArticle = (e) => {
     e.preventDefault();
-    
+
   }
-  
-  return(
-    <MainLayout>      
-      
+
+  return (
+    <MainLayout>
+
       <section className="text-gray-700 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="lg:full mx-auto flex flex-wrap">
@@ -27,66 +24,66 @@ function Noticias({ posts, cats }){
             <div className="lg:w-1/4 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
               <h1>Tipo de árticulo</h1>
               <form onSubmit={handleChangeArticle}>
-                <label htmlFor="radNoticias"className="block uppercase cursor-pointer">
+                <label htmlFor="radNoticias" className="block uppercase cursor-pointer">
                   <input className="mr-2" name="articulo" checked type="radio" id="radNoticias" />
                   Noticias
                 </label>
-                <label htmlFor="radMedios"className="block uppercase cursor-pointer">
-                  <input  className="mr-2" name="articulo" type="radio" id="radMedios" />
+                <label htmlFor="radMedios" className="block uppercase cursor-pointer">
+                  <input className="mr-2" name="articulo" type="radio" id="radMedios" />
                   En los medios
                 </label>
-              </form>   
+              </form>
 
-              <h1 className="my-3">Categorias</h1>   
+              <h1 className="my-3">Categorias</h1>
               {
-                categorias.map( cat => (
+                categorias.map(cat => (
                   <CategoriesLeftSidebar
-                    key={ cat._id }
-                    cat={ cat }/>
+                    key={cat._id}
+                    cat={cat} />
                 ))
-              }                                                      
+              }
             </div>
             {/* Panel Cards */}
             <div className="lg:w-3/4 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0 -mt-4">
               <div className="flex">
-                <div className="w-full h-12">                  
-                1 de {total} resultados
+                <div className="w-full h-12">
+                  1 de {total} resultados
                 </div>
               </div>
-              <div className="flex flex-wrap -m-4">            
+              <div className="flex flex-wrap -m-4">
                 {
-                
-                noticias.map( post => (
-                  <PostCard
-                    key={ post._id }
-                    post={ post }
-                    cats = { categorias }
-                    bloque = {"md:w-1/3"}
+
+                  noticias.map(post => (
+                    <PostCard
+                      key={post._id}
+                      post={post}
+                      cats={categorias}
+                      bloque={"md:w-1/3"}
                     />
-                ))
+                  ))
+
                 }
               </div>
             </div>
-          </div>          
+          </div>
         </div>
-      </section>    
+      </section>
     </MainLayout>
   )
 }
 
-export async function getStaticProps() {  
-  
+export async function getStaticProps() {
+
   const res = await fetch("https://api.webflow.com/collections/5fa2c45087b41f0f9b713464/items?limit=9&offset=0&api_version=1.0.0&access_token=ed2770ed568f942e403fab9300fa760b97eadc3ea3bb5901e025deb8cd4cb3ee")
   const posts = await res.json()
-  
+
   const resCat = await fetch("https://api.webflow.com/collections/5fabfcf448583971dcbcc5c4/items?api_version=1.0.0&access_token=ed2770ed568f942e403fab9300fa760b97eadc3ea3bb5901e025deb8cd4cb3ee")
   const cats = await resCat.json()
-  
+
   return {
     props: {
       posts,
       cats
-      
     },
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
@@ -95,4 +92,4 @@ export async function getStaticProps() {
   }
 }
 
-export default Noticias 
+export default Noticias
--- pages/noticias/[...slug].js
+++ pages/noticias/[...slug].js
@@ -1,108 +1,104 @@
-import React, { useEffect, useState } from 'react';
+import React from "react";
+import { useRouter } from "next/router";
+import { MainLayout } from "../../components/ui/layout/MainLayout";
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
+// import parse from 'html-react-parser';
 
-import { useRouter } from 'next/router';
-import { MainLayout } from '../../components/ui/layout/MainLayout';
-// import formatDistanceFromNow from 'date-fns/formatDistanceToNow'
-import { format } from 'date-fns';
-
-import {es} from 'date-fns/locale';
-import parse from 'html-react-parser';
-
-
-function Noticia ({ post }) {
-  // console.log(post)
+function Noticia({ item }) {
   const router = useRouter();
-  //ojo: slug[0:categoria - 1:slug - 2:id]
-  // const { [0]:categoria } = router.query
-
-  // console.log(categoria)
-
-  if(router.isFallback){
-    return <div>Loading...</div>
-  };
-
-  // const { name, ["created-on"]:creacion, ["post-body"]:cuerpo, ['main-image']:ruta, categoria } = items;
-  
-  
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <MainLayout>
-      {/* <section className="text-gray-700 body-font">
-        <div className="container px-5 py-24 mx-auto" >
-          <div className="lg:full mx-auto flex flex-wrap" >
-            <div className="lg:w-1/6 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0" >
-              Lateral
+      <section className="text-gray-700 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="lg:full mx-auto flex flex-wrap">
+            <div className="lg:w-1/6 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+              <p>Redes sociales</p>
             </div>
-            <div className="lg:w-5/6 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0 -mt-4" >
-              <h1 className="text-blue-500 uppercase font-semibold text-base">{ categoria }</h1>
-              
-
-              <h1 className="font-serif text-4xl leading-tighter my-5"> {name} </h1>
-              
-
-              <h1 className="text-gray-500 text-base font-sans uppercase">{format(new Date(creacion), "d MMM yyyy" , {locale:es})}</h1>
-              
+            <div className="lg:w-5/6 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0 -mt-4">
+              <h1 className="text-blue-500 uppercase font-semibold text-base">
+                {item?.categoria}
+              </h1>
+              <h1 className="font-serif text-4xl leading-tighter my-5">
+                {" "}
+                {item?.name}{" "}
+              </h1>
               <div className="my-5">
-                <img src={ruta.url} alt=""/>
+                <h1 className="text-gray-500 text-base font-sans uppercase">
+                  {format(new Date(Date.now()), "d MMM yyyy", { locale: es })}
+                </h1>
+                {/* <img src={ item["main-image"].url } alt=""/> */}
+                <img src="/static/bg/bg2.png" alt="" />
               </div>
               <div className="text-lg">
-                { cuerpo != null && parse(cuerpo) }
-              </div>                                          
-              
+                {/* { parse(item["post-body"]) } */}
+                <p>
+                  {" "}
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Culpa, vitae? Obcaecati officiis odio ullam nemo dolore
+                  officia optio, veniam omnis natus, totam asperiores ea
+                  voluptatem at alias minima, nihil qui!{" "}
+                </p>
+              </div>
             </div>
-          </div>          
+          </div>
         </div>
       </section>
-      
-      */}
     </MainLayout>
   );
-};
-// This also gets called at build time
+}
 
 export async function getStaticProps({ params }) {
-  
-  const { slug } = params;
-  
-  if(slug?.length ){
-    try {
-      const res = await fetch(`https://api.webflow.com/collections/5fa2c45087b41f0f9b713464/items/5fb27f393864732211ba2a20?api_version=1.0.0&access_token=ed2770ed568f942e403fab9300fa760b97eadc3ea3bb5901e025deb8cd4cb3ee`);
-      const post = await res.json();
-      
-      return { props: { post }  };
-      
-      
-    } catch (error) {
-      // The Twitter API most likely died
-      console.error(error);
-      return { props: {} };
-    }
-  }
-  else{
-    return { props: {} };
-  }
-  
-  // console.log(id)
-  // const res = await fetch(`https://api.webflow.com/collections/5fa2c45087b41f0f9b713464/items/${id}?api_version=1.0.0&access_token=ed2770ed568f942e403fab9300fa760b97eadc3ea3bb5901e025deb8cd4cb3ee`);
-  // const post = await res.json();
-  // const [items] = post?.items;  
-  // return { props: { items }  };
-};
+  // console.log("\nparametros: " + JSON.stringify( params))
+  // console.log("\nparametros tamaño: " + Object.keys(params).length)
 
-export async function getStaticPaths() {  
+  if (Object.keys(params).length) {
+    const [, id] = params.slug;
 
-  const res = await fetch(`https://api.webflow.com/collections/5fa2c45087b41f0f9b713464/items?api_version=1.0.0&access_token=ed2770ed568f942e403fab9300fa760b97eadc3ea3bb5901e025deb8cd4cb3ee`);
+    const res = await fetch(
+      `https://api.webflow.com/collections/5fa2c45087b41f0f9b713464/items/${id}?api_version=1.0.0&access_token=ed2770ed568f942e403fab9300fa760b97eadc3ea3bb5901e025deb8cd4cb3ee`
+    );
+
+    const post = await res.json();
+
+    const [item] = post?.items;
+
+    return {
+      props: {
+        item,
+      },
+    };
+  }
+
+  return {
+    props: {
+      item: {
+        name: "",
+        categoria: "",
+      },
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(
+    `https://api.webflow.com/collections/5fa2c45087b41f0f9b713464/items?api_version=1.0.0&access_token=ed2770ed568f942e403fab9300fa760b97eadc3ea3bb5901e025deb8cd4cb3ee`
+  );
+
   const posts = await res.json();
-    
-  const paths = posts?.items.map(( {slug, _id} ) => ({
-    params: { slug: [slug, _id],}
-    }));
 
-  return { 
-    fallback: true ,
+  const paths = posts?.items.map(({ slug, _id }) => ({
+    params: { slug: [slug, _id] },
+  }));
+
+  return {
+    fallback: true,
     paths,
-  }    
-};
+  };
+}
 
-
-export default Noticia
+export default Noticia;
